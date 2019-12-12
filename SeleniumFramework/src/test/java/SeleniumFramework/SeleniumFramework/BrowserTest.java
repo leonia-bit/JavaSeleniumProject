4af94e43e2d06aa1e73afcbb9117e6c633280951
mkbc3 <mkbc3@192.168.1.6>
2019-12-12 17:45:59 +0200

    Comments removed

--- SeleniumFramework/src/test/java/SeleniumFramework/SeleniumFramework/BrowserTest.java
+++ SeleniumFramework/src/test/java/SeleniumFramework/SeleniumFramework/BrowserTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class BrowserTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		String projectPath = System.getProperty("user.dir");
 		System.out.println("Project path: "+ projectPath);
 		System.setProperty("webdriver.gecko.driver", projectPath+"\\drivers\\geckodriver\\geckodriver.exe");
