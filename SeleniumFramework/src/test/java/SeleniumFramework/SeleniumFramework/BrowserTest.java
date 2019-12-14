aeeeb226fbb2c555327aefe941ad0862bf72bf06
mkbc3 <mkbc3@192.168.1.6>
2019-12-14 18:14:39 +0200

    Add support for chrome driver. [n] at the end because chrome driver directory contain not stable driver and not deleted.

--- SeleniumFramework/src/test/java/SeleniumFramework/SeleniumFramework/BrowserTest.java
+++ SeleniumFramework/src/test/java/SeleniumFramework/SeleniumFramework/BrowserTest.java
@@ -2,6 +2,7 @@ package SeleniumFramework.SeleniumFramework;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrowserTest {
 
@@ -9,10 +10,13 @@ public class BrowserTest {
 		
 		String projectPath = System.getProperty("user.dir");
 		System.out.println("Project path: "+ projectPath);
-		System.setProperty("webdriver.gecko.driver", projectPath+"\\drivers\\geckodriver\\geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
-		
+	//	System.setProperty("webdriver.gecko.driver", projectPath+"\\drivers\\geckodriver\\geckodriver.exe");
+	//	WebDriver driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", projectPath+"/drivers/chromedrivern/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://selenium.dev/");
+		//driver.close();
+		//driver.quit();
 	}
 
 }
