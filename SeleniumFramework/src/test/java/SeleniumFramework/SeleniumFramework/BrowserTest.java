c914b70496c6b2277fc487358659ea871352a5a7
mkbc3 <mkbc3@192.168.1.6>
2019-12-15 20:44:22 +0200

    1.IE WebDriver added. 2. Code for launch IE browser added.

--- SeleniumFramework/src/test/java/SeleniumFramework/SeleniumFramework/BrowserTest.java
+++ SeleniumFramework/src/test/java/SeleniumFramework/SeleniumFramework/BrowserTest.java
@@ -2,18 +2,28 @@ package SeleniumFramework.SeleniumFramework;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrowserTest {
 
 	public static void main(String[] args) {
-		
+	// Path set	
 		String projectPath = System.getProperty("user.dir");
 		System.out.println("Project path: "+ projectPath);
+// run firefox
 	//	System.setProperty("webdriver.gecko.driver", projectPath+"\\drivers\\geckodriver\\geckodriver.exe");
 	//	WebDriver driver = new FirefoxDriver();
-		System.setProperty("webdriver.chrome.driver", projectPath+"/drivers/chromedrivern/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+//  run chrome
+	//	System.setProperty("webdriver.chrome.driver", projectPath+"/drivers/chromedrivern/chromedriver.exe");
+	//	WebDriver driver = new ChromeDriver();
+// run IE
+		System.setProperty("webdriver.ie.driver", projectPath+"/drivers/iedriver/IEDriverServer.exe");
+		WebDriver driver  = new InternetExplorerDriver();
+		
+	
+		
+		
 		driver.get("https://selenium.dev/");
 		try {
 			Thread.sleep(3000);
