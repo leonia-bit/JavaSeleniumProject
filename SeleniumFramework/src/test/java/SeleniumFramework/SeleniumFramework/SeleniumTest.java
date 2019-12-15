da628e5ac6cb3c065d49f68a37a2ccf3d15d7a0d
mkbc3 <mkbc3@192.168.1.6>
2019-12-15 21:43:46 +0200

    1. Example to run chromedriver version 2.36 add. 2. The chromedriver download and instaled by webdrivermanager.

--- SeleniumFramework/src/test/java/SeleniumFramework/SeleniumFramework/SeleniumTest.java
+++ SeleniumFramework/src/test/java/SeleniumFramework/SeleniumFramework/SeleniumTest.java
@@ -1,9 +1,17 @@
 package SeleniumFramework.SeleniumFramework;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SeleniumTest {
 	public static void main(String[] args) {
-		WebDriverManager.chromedriver().setup();
+		WebDriverManager.chromedriver().version("2.36").setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://google.com");
+		driver.close();
+		driver.quit();
 	}
 }
