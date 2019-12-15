52391277f32ff752042686f585f27332db3b50ab
mkbc3 <mkbc3@192.168.1.6>
2019-12-15 21:31:54 +0200

    1. WebDriverManager added

---
+++ SeleniumFramework/src/test/java/SeleniumFramework/SeleniumFramework/SeleniumTest.java
@@ -0,0 +1,9 @@
+package SeleniumFramework.SeleniumFramework;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SeleniumTest {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+	}
+}
