8f40a181c9589abfbadc4bff23520fb1257329e9
mkbc3 <mkbc3@192.168.1.6>
2019-12-15 21:55:43 +0200

    1. Example for run firefox using webdrivermanager added

--- SeleniumFramework/src/test/java/SeleniumFramework/SeleniumFramework/SeleniumTest.java
+++ SeleniumFramework/src/test/java/SeleniumFramework/SeleniumFramework/SeleniumTest.java
@@ -2,14 +2,18 @@ package SeleniumFramework.SeleniumFramework;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SeleniumTest {
 	public static void main(String[] args) {
-		WebDriverManager.chromedriver().version("2.36").setup();
-		WebDriver driver = new ChromeDriver();
-		
+		//	WebDriverManager.chromedriver().version("2.36").setup();
+		//	WebDriver driver = new ChromeDriver();
+
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver = new FirefoxDriver();
+
 		driver.get("https://google.com");
 		driver.close();
 		driver.quit();
