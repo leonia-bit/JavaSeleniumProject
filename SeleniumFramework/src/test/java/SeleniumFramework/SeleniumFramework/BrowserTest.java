994c057579373446ff11c4103c7259cbf8368388
mkbc3 <mkbc3@192.168.1.6>
2019-12-17 18:12:09 +0200

    1.Modular code added

--- SeleniumFramework/src/test/java/SeleniumFramework/SeleniumFramework/BrowserTest.java
+++ SeleniumFramework/src/test/java/SeleniumFramework/SeleniumFramework/BrowserTest.java
@@ -1,6 +1,6 @@
 package SeleniumFramework.SeleniumFramework;
 
-//import java.awt.List;
+
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -11,31 +11,53 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrowserTest {
+	static String projectPath;
+	static String browser;
+	static WebDriver driver;
+
 
 	public static void main(String[] args) {
 		// Path set	
-		String projectPath = System.getProperty("user.dir");
+		projectPath = System.getProperty("user.dir");
 		System.out.println("Project path: "+ projectPath);
-		// run firefox
-		//	System.setProperty("webdriver.gecko.driver", projectPath+"\\drivers\\geckodriver\\geckodriver.exe");
-		//	WebDriver driver = new FirefoxDriver();
-		//  run chrome
-		System.setProperty("webdriver.chrome.driver", projectPath+"/drivers/chromedrivern/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		// run IE
-		//System.setProperty("webdriver.ie.driver", projectPath+"/drivers/iedriver/IEDriverServer.exe");
-		//WebDriver driver  = new InternetExplorerDriver();
 		
+		setBrowser();
+		setBrowserConfig();
+		runTest();
+	}
 
+	// setBrowser
+	public static void setBrowser() {
+		browser = "Chrome";
+	}
+	// setBrowserConfig
+	public static void setBrowserConfig() {
+		if(browser.contains("Firefox")) {
+			System.setProperty("webdriver.gecko.driver", projectPath+"\\drivers\\geckodriver\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		if(browser.contains("Chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectPath+"/drivers/chromedrivern/chromedriver.exe");
+			driver = new ChromeDriver();
+		}
 
-
+		if(browser.contains("InternetExplorer")) {
+			System.setProperty("webdriver.ie.driver", projectPath+"/drivers/iedriver/IEDriverServer.exe");
+			driver  = new InternetExplorerDriver();
+		}
+	}
+	// runTest
+	public static void runTest() {
+		
 		driver.get("https://google.com/");
-		/*WebElement textBox =*/ driver.findElement(By.xpath("//input[@name='q']")).sendKeys("I love Java , Selenium, Maven Framework");
-		//textBox.sendKeys("Hi Lena!!! I love You!!!"); 
+		//----------> Find element by xpath and set keys
+		/*WebElement textBox =*/ driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Find input element by xpath");
+		//textBox.sendKeys("Find input element by xpath"); 
+		//---------->Find all input elements in the page
 		List<WebElement>elementList = driver.findElements(By.xpath("//input"));
 		int elementCount = elementList.size();
 		System.out.println("Element count: "+ elementCount);
-		
+
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
