f0ade443200d7679f53f5f62dba3e02d7b62488a
mkbc3 <mkbc3@192.168.1.6>
2019-12-23 13:15:57 +0200

    1. First test using TestNG test frame work added

--- SeleniumFramework/src/test/java/test/TestGoogleSearch.java
+++ SeleniumFramework/src/test/java/test/TestGoogleSearch.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TestGoogleSearch {
 	public static void main(String[] args) {
 		googleSerch();
+		
+		
 	}
 	/**
 	 * Description: insert text to search in input field and lick search button
